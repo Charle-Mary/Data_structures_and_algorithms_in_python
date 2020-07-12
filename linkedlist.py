ca72c9e6c96d15ad12ffe2f99c7a8a1b612cec7b
Adebiyi <charlesmaryayo@gmail.com>
2020-07-12 18:03:41 +0100

    removing data from LinkedList

--- linkedlist.py
+++ linkedlist.py
@@ -17,6 +17,24 @@ class LinkedList(object):
         else:
             newNode.nextNode = self.head
             self.head = newNode
+
+    def remove(self, data):
+        
+        if self.head is None:
+            return
+
+        self.size -= 1
+        currentNode = self.head
+        previousNode = None
+
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+
+        if previousNode is None:
+            self.head = currentNode.nextNode
+        else:
+            previousNode.nextNode = currentNode.nextNode
     
     def size(self):
         return self.size
@@ -47,4 +65,3 @@ class LinkedList(object):
         while actualNode is not None:
             print('%d' % actualNode.data)
             actualNode = actualNode.nextNode
-            
